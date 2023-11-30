d47ad9941959e038cbfe806b3c0d1bb374067d6c
Joshua Deck <joshua.deck@adesso.de>
2023-11-30 11:00:06 +0100

    second Commit

--- app/src/main/java/com/example/gittest/MainActivity.kt
+++ app/src/main/java/com/example/gittest/MainActivity.kt
@@ -7,6 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        println("Mein erster Commit")
+        println("Meine zweiter Commit --> ich habe Mist gebaut")
     }
 }
